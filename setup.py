ca1dd9582a0dccbb832ca21b6eec1d8fb53332f7
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-06-26 23:22:05 +0200

    Add setup

---
+++ setup.py
@@ -0,0 +1,33 @@
+import setuptools
+
+with open("README.md") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt") as fh:
+    install_requires = fh.read()
+
+NAME = "vs-mask"
+
+RELEASE = "0.0.0"
+
+setuptools.setup(
+    name=NAME,
+    version=RELEASE,
+    author="",
+    author_email="",
+    description="",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=["vsmask"],
+    url="",
+    package_data={
+        'vsmask': ['py.typed'],
+    },
+    install_requires=install_requires,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
