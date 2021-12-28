f44841f345731149457e95b311e6ee649c89cf68
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-12-28 22:47:14 +0100

    update setup file

--- setup.py
+++ setup.py
@@ -1,32 +1,37 @@
+from distutils.util import convert_path
+from typing import Any, Dict
+
 from setuptools import setup
 
-with open("README.md") as fh:
+meta: Dict[str, Any] = {}
+with open(convert_path('vsmask/_metadata.py'), encoding='utf-8') as f:
+    exec(f.read(), meta)
+
+with open('README.md', encoding='utf-8') as fh:
     long_description = fh.read()
 
-with open("requirements.txt") as fh:
+with open('requirements.txt', encoding='utf-8') as fh:
     install_requires = fh.read()
 
-NAME = "vs-mask"
-RELEASE = "0.0.0"
 
 setup(
-    name=NAME,
-    version=RELEASE,
-    author="",
-    author_email="",
-    description="",
+    name='vsmask',
+    version=meta['__version__'],
+    author=meta['__author__'],
+    author_email='',
+    description='Various masking tools for Vapoursynth',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    packages=["vsmask"],
-    url="",
+    long_description_content_type='text/markdown',
+    packages=['vsmask'],
+    url='',
     package_data={
         'vsmask': ['py.typed'],
     },
     install_requires=install_requires,
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
     zip_safe=False,
     python_requires='>=3.9',
