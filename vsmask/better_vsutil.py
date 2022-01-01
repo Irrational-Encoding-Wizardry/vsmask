dff543092bebe930fb1b855fcdf3f190760284da
Ichunjo <ichunjo.le.terrible@gmail.com>
2022-01-01 22:45:26 +0100

    improve code

--- vsmask/better_vsutil.py
+++ vsmask/better_vsutil.py
@@ -1,16 +1,18 @@
 from typing import List, cast
 
 import vapoursynth as vs
+from vsutil import disallow_variable_format
+
+from .types import ensure_format
 
 core = vs.core
 
 
 def join(planes: List[vs.VideoNode], /, family: vs.ColorFamily = vs.YUV) -> vs.VideoNode:
     return planes[0] if len(planes) == 1 and family == vs.GRAY \
-        else core.std.ShufflePlanes(clips=planes[:3], planes=[0, 0, 0], colorfamily=family)
+        else core.std.ShufflePlanes(planes[:3], [0, 0, 0], family)
 
 
+@disallow_variable_format
 def split(clip: vs.VideoNode, /) -> List[vs.VideoNode]:
-    if clip.format is None:
-        raise ValueError('split: Variable format not allowed!')
-    return [clip] if clip.format.num_planes == 1 else cast(List[vs.VideoNode], clip.std.SplitPlanes())
+    return [clip] if ensure_format(clip).format.num_planes == 1 else cast(List[vs.VideoNode], clip.std.SplitPlanes())
