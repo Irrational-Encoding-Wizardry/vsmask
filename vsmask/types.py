c8a05c0a6f58d46d7750fba9b039a84ca46e9e36
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-11-12 16:23:33 +0100

    Remove unused types

--- vsmask/types.py
+++ vsmask/types.py
@@ -1,28 +1,12 @@
-
 from __future__ import annotations
 
-from typing import TYPE_CHECKING, Optional, Protocol, Sequence
+from typing import Protocol
 
-from vapoursynth import VideoFormat, VideoNode
-
-if TYPE_CHECKING:
-    class _VideoNode(VideoNode):
-        format: VideoFormat
-
-    def ensure_format(clip: VideoNode) -> _VideoNode:
-        return _VideoNode()
-else:
-    def ensure_format(clip: VideoNode) -> VideoNode:
-        return clip
+from vstools import PlanesT, StrArrOpt, VideoNode
 
 
 class MorphoFunc(Protocol):
-    def __call__(self, clip: VideoNode, planes: int | Sequence[int] | None = ...,
-                 threshold: Optional[int] = ..., coordinates: int | Sequence[int] | None = ...) -> VideoNode:
-        ...
-
-
-class ZResizer(Protocol):
-    def __call__(self, clip: VideoNode, width: Optional[int] = ..., height: Optional[int] = ...,
-                 format: Optional[int] = ...) -> VideoNode:
+    def __call__(
+        self, clip: VideoNode, planes: PlanesT = ..., threshold: int | None = ..., coordinates: StrArrOpt[int] = ...
+    ) -> VideoNode:
         ...
