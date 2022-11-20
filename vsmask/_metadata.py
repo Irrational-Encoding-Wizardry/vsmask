50c1274bd2ae03b5ac202b2d166c7510b633bab2
Ichunjo <ichunjo.le.terrible@gmail.com>
2022-11-20 21:58:20 +0100

    bump version

--- vsmask/_metadata.py
+++ vsmask/_metadata.py
@@ -1,5 +1,5 @@
 __author__ = 'IEW'
 
-__version__ = '0.4.0'
+__version__ = '0.5.0'
 __author__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
 __maintainer__, __maintainer_email__ = 'Ichunjo', __author_email__
