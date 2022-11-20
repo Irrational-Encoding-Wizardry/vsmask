9b0b3d89858a0c5c9cfe81634169db75636878bd
Ichunjo <ichunjo.le.terrible@gmail.com>
2022-11-20 21:52:52 +0100

    complete package metadatas

--- vsmask/_metadata.py
+++ vsmask/_metadata.py
@@ -1,4 +1,5 @@
 __author__ = 'IEW'
 
 __version__ = '0.4.0'
-__maintainer__ = 'IEW'
+__author__, __author_email__ = 'Irrational Encoding Wizardry', 'wizards@encode.moe'
+__maintainer__, __maintainer_email__ = 'Ichunjo', __author_email__
